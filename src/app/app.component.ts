0059a29f338119d042a85c30006a8c3c425fd021
ftrabol <f.trabol@gmail.com>
2018-05-07 18:49:16 -0400

    guardado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'angular crud';
+  employes =[
+    {name:'Fast',position:'manager'},
+    {name:'juna',position:'manager'},
+    {name:'pedro',position:'manager'},
+  ];
+
+  model:any={};
+  model2:any={};
+
+myvalue;
+  addEmployee():void{
+    this.employes.push(this.model);
+
+  }
+  deteleEmployee(i):void{
+
+      var aswer = confirm("esta reguro de eleimnar");    
+      if(aswer){
+          this.employes.splice(i,1);
+      }
+  }
+  
+  editEmployee(i):void{
+    this.model2.name     = this.employes[i].name;
+    this.model2.position = this.employes[i].position;
+    this.myvalue = i;
+  }
+
+  updateEmployee():void{
+    
+    let i = this.myvalue;
+    for(let j = 0; j < this.employes.length; j++){
+      if(i == j) {
+        this.employes[i] = this.model2;
+        this.model2 = {};
+      }
+    }
+
+  }
 }
